c11bfcdb1b8a613b22dcfc11e09cd39f8172a1ad
Rodrigo de Bona Sartor <rodrigo@kugel.com.br>
2016-07-20 16:30:49 -0300

    prevent confirm from been called more than one time

--- bootbox.js
+++ bootbox.js
@@ -100,16 +100,19 @@
     e.stopPropagation();
     e.preventDefault();
 
-    // by default we assume a callback will get rid of the dialog,
-    // although it is given the opportunity to override this
+    if( !dialog.closed ){ // ISSUE: https://github.com/makeusabrew/bootbox/issues/514
+      // by default we assume a callback will get rid of the dialog,
+      // although it is given the opportunity to override this
 
-    // so, if the callback can be invoked and it *explicitly returns false*
-    // then we'll set a flag to keep the dialog active...
-    var preserveDialog = $.isFunction(callback) && callback.call(dialog, e) === false;
+      // so, if the callback can be invoked and it *explicitly returns false*
+      // then we'll set a flag to keep the dialog active...
+      var preserveDialog = $.isFunction(callback) && callback.call(dialog, e) === false;
 
-    // ... otherwise we'll bin it
-    if (!preserveDialog) {
-      dialog.modal("hide");
+      // ... otherwise we'll bin it
+      if (!preserveDialog) {
+        dialog.closed = true;
+        dialog.modal("hide");
+      }
     }
   }
 
